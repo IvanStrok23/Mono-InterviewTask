b30430d03e6fc686a09400cb0853838732af47e3
ivans <ivan.strok.develop@gmail.com>
2021-01-10 14:10:49 +0100

    solution clean up

--- MonoTask/MonoTask.Core.Entities/Helpers/TableFilterData.cs
+++ MonoTask/MonoTask.Core.Entities/Helpers/TableFilterData.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MonoTask.UI.Web.Helper
+﻿namespace MonoTask.UI.Web.Helper
 {
     public class TableFilterData
     {
